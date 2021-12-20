a216784c2ed43a9052bc66a3e9a2d5f98fbf3cb8
LabaRuby <92020357+LabaRuby@users.noreply.github.com>
2021-12-21 02:40:18 +0300

    Update Laba_1_spec.rb

--- Laba_1_spec.rb
+++ Laba_1_spec.rb
@@ -1,16 +1,16 @@
 require 'rspec'
-require './lab1.rb'
+require './Laba_1.rb'
 
 RSpec.describe "Main" do
 
   it "#greeting_test_1" do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('Kirill', 'Biller', 17)
-    expect(greeting).to eq("Hi, Kirill Biller. You are less then 18, but starting programming is never early")
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('Sonya', 'Kuznetsova', 14)
+    expect(greeting).to eq("Hi, Sonya Kuznetsova. You are less then 18, but starting programming is never early")
   end
 
   it "#greeting_test_2" do
-    allow_any_instance_of(Kernel).to receive(:gets).and_return('Kirill', 'Biller', 20)
-    expect(greeting).to eq("Hello, Kirill Biller. It's time to get busy")
+    allow_any_instance_of(Kernel).to receive(:gets).and_return('Sonya', 'Kuznetsova', 22)
+    expect(greeting).to eq("Hello, Sonya Kuznetsova. It's time to get busy")
   end
 
   it "#foobar_test_1" do
